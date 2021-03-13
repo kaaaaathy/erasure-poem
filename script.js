72ad5dcbc096a17586baccba64e958b62ea725cf
Glitch (hello-webpage) <none>
2021-03-13 22:18:26 +0000

    👍🚣 Checkpoint ./script.js:767229/1883 ./style.css:767229/196 ./index.html:767229/9

--- script.js
+++ script.js
@@ -3,56 +3,62 @@
 //—————————————————————————————————————————
 
   let fruits = ["orange","strawberry","durian","pineapple", "guava"];
-  
+
   //introduce Array + For loop
   for (let i = 0; i < fruits.length; i++){
       console.log("i is " + i);
       console.log("the " + i + " number fruit is " + fruits[i]);
   }
-    
-    
-  //introduce
-  for (let i = 0; i < fruits.length; i++){
+
+
+  let string = "Two roads diverged in a yellow wood, and sorry I could not travel both, and be one traveler, long I stood and looked down one as far as I could, to where it bent in the undergrowth"
+  let stringArray = string.split(' ');
+  console.log(stringArray);
+
+
+  //introduce Create Element + Append
+  for (let i = 0; i < stringArray.length; i++){
       console.log("i is " + i);
-      console.log("the " + i + " number fruit is " + fruits[i]);
+      console.log("the " + i + " number word is " + stringArray[i]);
     
     //creating new div
       let newdiv = document.createElement("div");
     
     //set inner HTML to the array
-      newdiv.innerHTML = fruits[i];
+      newdiv.innerHTML = stringArray[i];
     
     //you need to appendChild to add it to the body of the HTML doc.
-      document.body.appendChild(newdiv);
+      document.getElementById('content').appendChild(newdiv);
     
-    //this is a function to add a new class
+    //add class "wordItem" so you can add styling
+      newdiv.classList.add('wordItem'); 
+      
+    //this is a function to add class toggle behavior
       toggleClass(newdiv);
 
-    
   }
     
-    
-  function toggleClass(element){
-          element.classList.add('wordItem');
-          element.onclick = function(){
-          element.classList.toggle("read");
-          console.log("clicked");
-          }
+  //introduce ClassList
+  function toggleClass(element){    
+          element.onclick = function() {
+            element.classList.toggle("read");
+            console.log("clicked"); //tell me that it is clicked
+          } 
     }
 
 
-//—————————————————————————————————————————
-//    Example 1: Arrays + For Loops
-//—————————————————————————————————————————
+//—————————————————————————————————————————————————
+//    Example 2: Split an Array + Load a Text File
+//—————————————————————————————————————————————————
 
   let importedTxt;
   let words;
     
-  let string = "Two roads diverged in a yellow wood, and sorry I could not travel both, and be one traveler, long I stood and looked down one as far as I could, to where it bent in the undergrowth"
 
+//uncomment this to run it
+  // setup();
 
-    
-  //USE THIS CODE IF YOU WANT TO LOAD A TEXT FILE
+//USE THIS CODE IF YOU WANT TO LOAD A TEXT FILE
 //   function setup(){
 //     fetch('lumber.txt')
 //       .then(response => response.text())
@@ -71,6 +77,8 @@
   //     words = text.split(' ');
   //     console.log(words);
   // }
+  
+  
 
  // MAKE IT APPEAR IN DOM
 //   function build(){
@@ -85,6 +93,4 @@
 //     console.log("BUILD COMPLETE");
 
 //   }
-
-  setup();
 
